7da476fded8b172e894ac19b4a3ad857fdfdac10
ldvcbe <ldvc.be@gmail.com>
2026-02-23 20:06:48 +0100

    Passage sur profil VTT pour forcer le off-road gratuit

--- lib/routing_service.dart
+++ lib/routing_service.dart
@@ -14,39 +14,12 @@ class RoutingService {
   RoutingService(this.apiKey);
 
   Future<RouteData?> getOffRoadRoute(LatLng start, LatLng dest) async {
-    final url = 'https://graphhopper.com/api/1/route?key=$apiKey';
-    
-    final body = json.encode({
-      "points": [
-        [start.longitude, start.latitude],
-        [dest.longitude, dest.latitude]
-      ],
-      "profile": "car",
-      "locale": "fr",
-      "instructions": true,
-      "elevation": false,
-      "points_encoded": false,
-      "ch.disable": true, // Désactive le cache rapide pour utiliser notre recette
-      "custom_model": {
-        // Autorise de plus longs détours géographiques pour trouver de la piste (défaut: 70)
-        "distance_influence": 40,
-        "priority": [
-          // On fuit les autoroutes et voies rapides
-          { "if": "road_class == MOTORWAY || road_class == TRUNK", "multiply_by": 0.05 },
-          // L'asphalte reste possible pour les liaisons, mais fortement pénalisé face à la terre
-          { "if": "surface == ASPHALT || surface == PAVED || surface == CONCRETE", "multiply_by": 0.3 },
-          // On traverse moins les lotissements et zones résidentielles
-          { "if": "road_class == RESIDENTIAL", "multiply_by": 0.6 }
-        ]
-      }
-    });
+    // LE HACK 4x4 : On utilise le profil Mountain Bike (VTT) !
+    // Il est gratuit et sa priorité absolue est de fuir le goudron pour trouver de la terre.
+    final url = 'https://graphhopper.com/api/1/route?point=${start.latitude},${start.longitude}&point=${dest.latitude},${dest.longitude}&profile=mtb&key=$apiKey&instructions=true&points_encoded=false';
 
     try {
-      final response = await http.post(
-        Uri.parse(url),
-        headers: {"Content-Type": "application/json"},
-        body: body,
-      );
+      final response = await http.get(Uri.parse(url));
       
       if (response.statusCode == 200) {
         final data = json.decode(response.body);
@@ -58,7 +31,7 @@ class RoutingService {
           return RouteData(points, instructions);
         }
       } else {
-        return RouteData([start, dest], [{'text': 'ERREUR API: ${response.statusCode} - ${response.body}', 'interval': [0, 1]}]);
+        return RouteData([start, dest], [{'text': 'ERREUR API: ${response.statusCode}', 'interval': [0, 1]}]);
       }
     } catch (e) {
       return RouteData([start, dest], [{'text': 'CRASH INTERNE: $e', 'interval': [0, 1]}]);
