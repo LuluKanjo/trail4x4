b3b11c012482416d8e6fa893f8d76ac24fef5070
ldvcbe <ldvc.be@gmail.com>
2026-02-25 19:01:43 +0100

    Radar POI : Passage sur Overpass API OpenSource

--- lib/poi_service.dart
+++ lib/poi_service.dart
@@ -1,42 +1,59 @@
+import 'package:latlong2/latlong.dart';
+import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'package:flutter/foundation.dart';
-import 'package:http/http.dart' as http;
-import 'package:latlong2/latlong.dart';
 
 class POI {
-  final String name, type;
   final LatLng position;
-  POI({required this.name, required this.type, required this.position});
+  final String type;
+  final String name;
+
+  POI(this.position, this.type, this.name);
 }
 
 class POIService {
-  final String tomtomKey;
-  POIService({required this.tomtomKey});
+  // On garde tomtomKey dans les paramètres pour ne pas faire planter main.dart
+  // mais on ne s'en sert plus ! On passe au 100% Gratuit/OpenSource.
+  POIService({String? tomtomKey});
 
   Future<List<POI>> fetchPOIs(double lat, double lon, String type) async {
-    if (type == 'fuel') {
-      final url = 'https://api.tomtom.com/search/2/search/petrol station.json?lat=$lat&lon=$lon&radius=30000&key=$tomtomKey';
-      try {
-        final res = await http.get(Uri.parse(url));
-        if (res.statusCode == 200) {
-          final data = json.decode(res.body);
-          final List results = data['results'] ?? [];
-          return results.map((r) => POI(name: r['poi']['name'] ?? 'Station', type: type, position: LatLng(r['position']['lat'], r['position']['lon']))).toList();
+    List<POI> results = [];
+    
+    // Traduction de nos boutons en langage OpenStreetMap
+    String osmTag = '';
+    if (type == 'gas') osmTag = '["amenity"="fuel"]';
+    if (type == 'water') osmTag = '["amenity"="drinking_water"]';
+    
+    // On lance un scan radar sur un rayon de 15 km (15000 mètres)
+    String query = '''
+      [out:json][timeout:15];
+      (
+        node$osmTag(around:15000,$lat,$lon);
+        way$osmTag(around:15000,$lat,$lon);
+      );
+      out center;
+    ''';
+
+    try {
+      // Appel au serveur mondial public Overpass
+      final url = Uri.parse('https://overpass-api.de/api/interpreter');
+      final response = await http.post(url, body: query);
+
+      if (response.statusCode == 200) {
+        final data = json.decode(response.body);
+        for (var element in data['elements']) {
+          // Gère les points (node) et les bâtiments (way)
+          double pLat = element['type'] == 'node' ? element['lat'] : element['center']['lat'];
+          double pLon = element['type'] == 'node' ? element['lon'] : element['center']['lon'];
+          String name = element['tags']?['name'] ?? (type == 'gas' ? 'Station Essence' : 'Point d\'eau');
+          
+          results.add(POI(LatLng(pLat, pLon), type, name));
         }
-      } catch (e) { debugPrint("Erreur POI fuel: $e"); }
-    } else {
-      String query = type == 'water' 
-        ? '[out:json];node(around:15000,$lat,$lon)["amenity"~"drinking_water|water_point|fountain"];out;'
-        : '[out:json];node(around:15000,$lat,$lon)["tourism"~"camp_site|picnic_site"];out;';
-      try {
-        final res = await http.get(Uri.parse('https://overpass-api.de/api/interpreter?data=$query'));
-        if (res.statusCode == 200) {
-          final data = json.decode(res.body);
-          final List elements = data['elements'] ?? [];
-          return elements.map((e) => POI(name: e['tags']['name'] ?? (type == 'water' ? 'Eau' : 'Bivouac'), type: type, position: LatLng(e['lat'], e['lon']))).toList();
-        }
-      } catch (e) { debugPrint("Erreur POI Overpass: $e"); }
+      }
+    } catch (e) {
+      debugPrint("Erreur Radar POI: $e");
     }
-    return [];
+    
+    return results;
   }
 }
